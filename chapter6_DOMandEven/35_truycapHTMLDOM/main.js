8b1d41400d40241ad0f5c46e2080f9bb4504ddfb
Mrlong8 <xuanlong1806@gmail.com>
2026-06-02 15:00:10 +0700

    hoc ve git

---
+++ chapter6_DOMandEven/35_truycapHTMLDOM/main.js
@@ -0,0 +1,10 @@
+console.log("Hello work")
+
+const cnt = document.getElementById("drayonbx")
+
+console.log(cnt)
+
+const myDiv = document.querySelector(".helloWordl")
+
+const allMyDivs = document.querySelectorAll(".helloWordl")
+console.log(allMyDivs)
